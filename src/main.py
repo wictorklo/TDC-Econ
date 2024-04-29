c1db3f84ed60b6f03739b55146f24102f0bfae69
WildIAM <william.v.carstensen@gmail.com>
2024-04-29 18:26:05 +0200

    Added fun, formatting, some dynamic price adjustments, and login validation

--- src/main.py
+++ src/main.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template, request, make_response, url_for, redirect, jsonify
+from flask import (
+    Flask,
+    render_template,
+    request,
+    make_response,
+    url_for,
+    redirect,
+    jsonify,
+)
 import json
 
 # https://marketsplash.com/how-to-use-flask-with-websockets/
 
+price_constant = 0.02
+
 activities = {}
 users = {}
 admins = []
+log = []
+stock_prices = {}
 
 with open("config.json", "r") as file:
     cfg = json.load(file)
@@ -13,86 +25,128 @@ with open("config.json", "r") as file:
     print(cfg)
     activities = cfg["activities"]
     admins = cfg["admins"]
-    for p in cfg["people"]:
+    for i, p in enumerate(cfg["people"]):
         users[p] = {}
         users[p]["money"] = cfg["start_cash"]
         users[p]["bets"] = []
         users[p]["stocks"] = {}
+        users[p]["password"] = cfg["passwords"][i]
         for i in cfg["people"]:
-             users[p]["stocks"][i] = 0
-        print(p, users[p])
+            users[p]["stocks"][i] = 0
+            stock_prices[i] = 1
+        # print(p, users[p])
 
 
+app = Flask(__name__, static_url_path="/static")
 
-app = Flask(__name__, static_url_path='/static')
 
 def bet(better, activity, target, time):
     pass
 
+
 def stockPrice(name):
-    #TODO: Do this dynamically
-    return 1
+    return stock_prices[name]
+
+
+def upPrice(name, amount=1):
+    """Adjust the price of a stock upwards"""
+    for _ in range(amount):
+        stock_prices[name] = round(stock_prices[name] * (1 + price_constant), 4)
+    print(name, "new price: ", stock_prices[name])
+
+
+def downPrice(name, amount=1):
+    """Adjust the price of a stock downwards"""
+    for _ in range(amount):
+        stock_prices[name] = round(stock_prices[name] / (1 + price_constant), 4)
+    print(name, "new price: ", stock_prices[name])
+
 
 @app.route("/")
 def index():
     name = request.cookies.get("name")
     print(name, name == None)
     if name == None:
-        return redirect(url_for('static', filename='login.html'))
+        return redirect(url_for("static", filename="login.html"))
     if name in users.keys():
-        return render_template("index.html", name=name, user=users[name], activities=activities, users=users)
-    return redirect(url_for('static', filename='login.html'))
+        return render_template(
+            "index.html",
+            name=name,
+            user=users[name],
+            activities=activities,
+            users=users,
+        )
+    return redirect(url_for("static", filename="login.html"))
+
 
 @app.post("/login")
 def login():
     print(request.form)
     if validateLogin(request.form["name"], request.form["code"]):
+        print(True)
         resp = make_response(redirect("/"))
         resp.set_cookie("name", request.form["name"])
         return resp
-    return redirect(url_for('static', filename='login.html'))
+    return redirect(url_for("static", filename="login.html"))
+
 
 @app.get("/dashboard")
 def overview():
     return render_template("overview.html")
 
+
 @app.get("/person/<name>")
 def person(name=None):
-    #TODO: Check that user does not try to invest in themselves
-    #Or leave it in as infinite money glitch?
+    # TODO: Check that user does not try to invest in themselves
+    # Or leave it in as infinite money glitch?
     return render_template("player.html", user=users[name])
+
 
 @app.post("/person/<name>/buy")
 def invest(name=None):
     print(request.json)
     amount = request.json["amount"]
     buyer = request.cookies.get("name")
-    print(f"{buyer} bought {amount} stocks in {name}")
-    # TODO: Check if buyer can afford it
-    users[buyer]["stocks"][name] += amount
-    users[buyer]["money"] -= amount*stockPrice(name)
+    tot_price = round(amount * stockPrice(name), 2)
+    if users[buyer]["money"] > tot_price:
+        print(f"{buyer} bought {amount} stocks in {name}")
+        users[buyer]["stocks"][name] += amount
+        users[buyer]["money"] -= tot_price
+        upPrice(name, amount)
+    else:
+        pass
+        # TODO: allert player they don't have enough money to buy
     # TODO: Add to running log
     # TODO: Send update to all players
     return redirect("/")
+
 
 @app.post("/person/<name>/sell")
 def sell(name=None):
     print(request.json)
     amount = request.json["amount"]
     seller = request.cookies.get("name")
-    print(f"{seller} bought {amount} stocks in {name}")
-    # TODO: Check if seller owns the stocks
-    users[seller]["stocks"][name] -= amount
-    users[seller]["money"] += amount*stockPrice(name)
+    if users[seller]["stocks"][name]:
+        print(f"{seller} sold {amount} stocks in {name}")
+        users[seller]["stocks"][name] -= amount
+        users[seller]["money"] += round(amount * stockPrice(name), 2)
+        downPrice(name, amount)
+    else:
+        pass
+        # TODO: allert player they don't own the stock
     # TODO: Add to running log
     # TODO: Send update to all players
     return redirect("/")
+
 
 @app.get("/activity/<activity>")
 def activity(activity=None):
     return render_template("activity.html", activity=activities[activity])
 
+
 app.post("/activity/<activity>/bet")
+
+
 def bet(activity=None):
     amount = request.form["amount"]
     player = request.form["player"]
@@ -103,6 +157,7 @@ def bet(activity=None):
     bet(better, activity, player, time)
     return redirect("/")
 
+
 @app.get("/admin")
 def adminMenu(name=None):
     name = request.cookies.get("name")
@@ -110,6 +165,10 @@ def adminMenu(name=None):
         return render_template("admin.html", user=users[name])
     return redirect("/")
 
+
 def validateLogin(name, password):
-    #TODO: Add some basic logic here so people dont fuck each other over...
-    return True
+    """Validate login information"""
+    if name in users:
+        if users[name]["password"] == password:
+            return True
+    return False
